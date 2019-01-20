32b3cdd096137f2d82e4a1c454c7791c13672460
doomsower <K.Kuznetcov@gmail.com>
2019-01-20 12:56:24 +0300

    build: add controlled storybook

--- src/Editor.stories.tsx
+++ src/Editor.stories.tsx
@@ -2,6 +2,26 @@ import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import MdEditor from './MdEditor';
+import { MdEditorValue } from './types';
+import { fromMarkdown } from './utils';
+
+interface State {
+  value: MdEditorValue;
+}
+
+class Controller extends React.PureComponent<{}, State> {
+  readonly state: State = { value: fromMarkdown('Hello **new** world!') };
+
+  onChange = (value: MdEditorValue) => {
+    this.setState({ value });
+  };
+
+  render() {
+    return (
+      <MdEditor value={this.state.value} onChange={this.onChange} />
+    );
+  }
+}
 
 storiesOf('MdEditor', module)
   .addDecorator(story => (
@@ -12,5 +32,10 @@ storiesOf('MdEditor', module)
   .add('default', () => {
     return (
       <MdEditor />
+    );
+  })
+  .add('controlled', () => {
+    return (
+      <Controller />
     );
   });
